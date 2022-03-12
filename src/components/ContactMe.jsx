59d24d096d68e0ea55c0ac4c168ef33d12057e2d
ahmed <abdallaaymanshandi3@gmail.com>
2022-03-12 09:54:10 +0200

    c

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -39,7 +39,7 @@ function ContactMe() {
   };
   return (
     <div
-      className="py-32 flex flex-col justify-center   font-bold	 text-white"
+      className="pt-20 pb-32  flex flex-col justify-center   font-bold	 text-white"
       id="contact"
     >
       <div className="flex items-center my-8" data-aos="zoom-out-right">
